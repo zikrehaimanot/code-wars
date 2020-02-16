30010d9376c9798812e6ebde16e51a5100e4fea5
zikrehaimanot <zikrehaimanot@gmail.com>
2020-02-15 22:53:20 -0500

    code wars 5

---
+++ js/mainfive.js
@@ -0,0 +1,15 @@
+
+//function opposite(number) {
+//  if (number <0){
+//  return number
+//  }else{
+//  number= number * -1
+//  }
+// }
+
+// QUESTION: Very simple, given a number, find its opposite.
+
+
+function opposite(number) {
+  return (-number)
+}
---
+++ js/mainthree.js
@@ -0,0 +1,13 @@
+
+//
+// question:You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+
+const areaOrPerimeter = function(l , w) {
+   if (l ==w){
+   return (l * w)}
+   else{
+   return((2*l)+ (2 *w))
+   }
+};
